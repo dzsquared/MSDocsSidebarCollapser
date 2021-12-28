d192180f988c905183d23c4e796c70360adafeeb
Drew Skwiers-Koballa <dzsquared@users.noreply.github.com>
2021-12-28 22:22:43 +0000

    working on more flexible placement

--- buttonActions.js
+++ buttonActions.js
@@ -1,14 +1,28 @@
 function collapseSide() {
-    $('.left-container').toggle(500);
-    $('.primary-holder').animate({ width: '100%' });
-    document.getElementById("docController").setAttribute("currentSidebar", "revealSide");
-    document.getElementById("docController").innerHTML = "Show Sidebar";
+    // find class mainContainer
+    var bodyContainer = document.getElementsByClassName("columns")[0];
+    if (typeof bodyContainer !== 'undefined') {
+        var openerHolder = document.createElement("DIV");
+        openerHolder.setAttribute("class", "column left-container");
+        openerHolder.setAttribute("id", "sidebarHolder");
+        openerHolder.innerHTML = "<button class='button link-button' id='docControllerSidebar' data-bi-name='sidebarController' onclick='sidebarButton()' currentSidebar='revealSide' style='margin-bottom: 0.5em;margin-top: 0.5em;'>Show Sidebar</button>";
+
+        $('.left-container').toggle(500);
+        bodyContainer.insertBefore(openerHolder, bodyContainer.childNodes[0]);
+        $('.primary-holder').animate({ width: '100%' });
+
+        document.getElementById("docController").setAttribute("currentSidebar", "revealSide");
+        document.getElementById("docController").innerHTML = "Show Sidebar";
+    }
 }
 function revealSide() {
+    document.getElementById("sidebarHolder").remove();
     $('.left-container').toggle(500);
     $('.primary-holder').animate({ width: '75%' });
     document.getElementById("docController").setAttribute("currentSidebar", "collapseSide");
     document.getElementById("docController").innerHTML = "Collapse Sidebar";
+    document.getElementById("docControllerSidebar").setAttribute("currentSidebar", "collapseSide");
+    document.getElementById("docControllerSidebar").innerHTML = "Collapse Sidebar";
 }
 
 function sidebarButton() {
--- fullWidth.js
+++ fullWidth.js
@@ -12,12 +12,22 @@ function setupDocs() {
     };
     (document.head || document.documentElement).appendChild(s);
 
-    // prepand a button to the page metadata area
+    // prepend a button to the page metadata area
     var pageMetadata = document.getElementsByClassName("page-metadata")[0];
     if (typeof pageMetadata !== 'undefined') {
         var sidebarButton = document.createElement("LI");
-        sidebarButton.innerHTML = "<button class='button is-small' id='docController' data-bi-name='sidebarController' onclick='sidebarButton()' currentSidebar='collapseSide' style='margin-bottom: 0.5em;margin-top: 0.5em;'>Collapse Sidebar</button>"
+        sidebarButton.innerHTML = "<button class='button link-button' id='docController' data-bi-name='sidebarController' onclick='sidebarButton()' currentSidebar='collapseSide' style='margin-bottom: 0.5em;margin-top: 0.5em;'>Collapse Sidebar</button>"
         pageMetadata.insertBefore(sidebarButton, pageMetadata.childNodes[0]);
+    }
+
+    // prepend a button to the sidebar
+    var sidebar = document.getElementsByClassName("left-container")[0];
+    if (typeof sidebar !== 'undefined') {
+        var sidebarButton = document.createElement("DIV");
+        // sidebarButton.setAttribute("class", "column left-container");
+        sidebarButton.setAttribute("id", "sidebarButtonHolder");
+        sidebarButton.innerHTML = "<button class='button is-small' id='docControllerSidebar' data-bi-name='sidebarController' onclick='sidebarButton()' currentSidebar='collapseSide' style='margin-bottom: 0.5em;margin-top: 0.5em;'>Collapse Sidebar</button>"
+        sidebar.insertBefore(sidebarButton, sidebar.childNodes[0]);
     }
 }
 setupDocs();
